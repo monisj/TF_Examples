accbe19a07538ebeda8950d3111f25eeec170228
monisj <monisj94@gmail.com>
2023-01-27 16:41:29 +0500

    Scaling Multiple Images in database

--- First_NN.py
+++ First_NN.py
@@ -13,8 +13,20 @@ class_names=['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt',
 # print(test_images.shape)
 # print(len(test_labels))
 
-PLT.figure()
-PLT.imshow(train_images[1])# Changing the index changes the image being viewed 
-PLT.colorbar()
-PLT.grid(False)
+# PLT.figure()
+# PLT.imshow(train_images[1])# Changing the index changes the image being viewed 
+# PLT.colorbar()
+# PLT.grid(False)
+# PLT.show()
+
+train_images=train_images/255.0
+test_images=test_images/255.0
+PLT.figure(figsize=(10,10))
+for image_loop in range(30):
+    PLT.subplot(5,6,image_loop+1)
+    PLT.xticks([])
+    PLT.yticks([])
+    PLT.grid(False)
+    PLT.imshow(train_images[image_loop],cmap=PLT.cm.binary)
+    PLT.xlabel(class_names[train_labels[image_loop]])
 PLT.show()
