92b3a53fc3939320dbae28b40dd24c00c136503c
monisj <monisj94@gmail.com>
2023-01-27 16:29:15 +0500

    Exploring Labels of train images

--- First_NN.py
+++ First_NN.py
@@ -2,3 +2,9 @@ import tensorflow as TF
 import numpy as np
 import matplotlib.pyplot as PLT
 print(TF.__version__)
+fashion_m=TF.keras.datasets.fashion_mnist
+(train_images,train_labels),(test_images,test_labels)=fashion_m.load_data()
+
+class_names=['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
+print(train_images.shape)
+print(len(train_labels))
